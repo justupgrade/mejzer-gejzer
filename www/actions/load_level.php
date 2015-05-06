7057a4ebe1b863b6d26d5732a46b1f56ad670fe0
Tomasz <justupgrade@o2.pl>
2015-05-06 12:19:44 +0200

    updated structure of project: maps are loaded from different files now

--- www/actions/load_level.php
+++ www/actions/load_level.php
@@ -4,12 +4,12 @@
  * User: tomasz
  * Date: 26.04.15
  * Time: 13:24
- */
-    require_once '../classes/Map.php';
+ */ 
+    $raw = json_decode(file_get_contents("../data/map.json"), true);
 
     $lvl = isset($_POST['lvl']) ? $_POST['lvl'] : 0;
-    $map = new Map($lvl);
-    $map->load();
-    echo json_encode($map->getRoom());
+    $path = "../data/maps/lvl".$lvl.".json";
+    
+    echo file_get_contents($path);
     die();
 ?>
